a24f478ce6434e067410e2e994d5effdb41e5058
csh0034 <csh0034@gmail.com>
2025-11-25 18:16:59 +0900

    refactor: ImportHttpServices basePackages 적용

--- src/main/kotlin/com/ask/springboot4/config/RestClientConfig.kt
+++ src/main/kotlin/com/ask/springboot4/config/RestClientConfig.kt
@@ -1,11 +1,8 @@
 package com.ask.springboot4.config
 
-import com.ask.springboot4.client.GitHubClient
-import com.ask.springboot4.client.GoogleClient
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.service.registry.ImportHttpServices
 
 @Configuration
-@ImportHttpServices(types = [GoogleClient::class])
-@ImportHttpServices(types = [GitHubClient::class])
+@ImportHttpServices(basePackages = ["com.ask.springboot4.client"])
 class RestClientConfig
